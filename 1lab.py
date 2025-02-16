7953a32b1abefd8950d3401252d42b87f00608a8
MatasSioma <matassemenas@gmail.com>
2025-02-16 15:17:38 +0200

    dalijimo pusiau metodas veikia

--- 1lab.py
+++ 1lab.py
@@ -5,10 +5,13 @@ import numpy as np
 # f(x) = (x^2−a)^2/b−1 -> (x^2)^2/4−1
 def tiklsoF(x):
     return pow(pow(x, 2), 2) / 3
+    # return pow(pow(x, 2) - 8, 2) / 7 #pvz su kitais a, b
+
 
 while True:
     try:
         intervalas = (int(input("pradinis rėžis (l): ")), int(input("galinis rėžis (r): ")))
+        epsilon = pow(10, int(input("ε = 10^")))
         l, r = intervalas[0], intervalas[1]
         if l > r:
             print("Klaida! 'l' yra mažiau negu 'r'.")
@@ -20,17 +23,30 @@ while True:
 xm = (l + r) / 2
 ilgis = r - l
 
-fxm = tiklsoF(xm)
-x1 = l + ilgis / 4
-x2 = r - ilgis / 4
+interacijos = 0
+while True:
+    interacijos += 1
 
-if tiklsoF(x1) < fxm:
-    pass
-elif tiklsoF(x2) < fxm:
-    pass
-else:
-    pass
+    fxm = tiklsoF(xm)
+    x1 = l + ilgis / 4
+    x2 = r - ilgis / 4
+
+    if tiklsoF(x1) < fxm:
+        r = xm
+        xm = x1
+    elif tiklsoF(x2) < fxm:
+        l = xm
+        xm = x2
+    else:
+        l = x1
+        r = x2
+
+    ilgis = r - l
+    if ilgis < epsilon:
+        break
 
 x = np.arange(0, 10, 0.1)
 pl.plot(x, tiklsoF(x))
+pl.plot(xm, tiklsoF(xm), 'or')
+print(interacijos)
 pl.show()
