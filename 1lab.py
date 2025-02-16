f54b65e8105590756c643c91951c673af98f9b13
MatasSioma <matassemenas@gmail.com>
2025-02-16 14:58:42 +0200

    Intervalo dalijimo pusiau metodo algoritmo progresas

--- 1lab.py
+++ 1lab.py
@@ -6,7 +6,31 @@ import numpy as np
 def tiklsoF(x):
     return pow(pow(x, 2), 2) / 3
 
-x = np.arange(0, 10, 0.1)
+while True:
+    try:
+        intervalas = (int(input("pradinis rėžis (l): ")), int(input("galinis rėžis (r): ")))
+        l, r = intervalas[0], intervalas[1]
+        if l > r:
+            print("Klaida! 'l' yra mažiau negu 'r'.")
+            continue
+        break
+    except ValueError:
+        print("Įvestis turi būti skaičius.")
 
+xm = (l + r) / 2
+ilgis = r - l
+
+fxm = tiklsoF(xm)
+x1 = l + ilgis / 4
+x2 = r - ilgis / 4
+
+if tiklsoF(x1) < fxm:
+    pass
+elif tiklsoF(x2) < fxm:
+    pass
+else:
+    pass
+
+x = np.arange(0, 10, 0.1)
 pl.plot(x, tiklsoF(x))
 pl.show()
