46860cf33385cf176261c25f29431ce634847a70
MatasSioma <matassemenas@gmail.com>
2025-02-16 17:23:27 +0200

    Auksinio pjuvio algoritmas

--- 1lab.py
+++ 1lab.py
@@ -1,7 +1,7 @@
 #2314004 - stud knygeles nr
 import matplotlib.pyplot as pl
 import numpy as np
-import copy
+import copy, math
 
 # f(x) = (x^2−a)^2/b−1 -> (x^2)^2/4−1
 def tiklsoF(x):
@@ -20,14 +20,14 @@ while True:
         print("Įvestis turi būti skaičius.")
 
 def dalijimoPusiau():
-    global intervalas
+    global intervalas, epsilon
     l, r = copy.deepcopy(intervalas[0]), copy.deepcopy(intervalas[1])
     xm = (l + r) / 2
     ilgis = r - l
 
-    interacijos = 0
+    iteracijos = 0
     while True:
-        interacijos += 1
+        iteracijos += 1
 
         fxm = tiklsoF(xm)
         x1 = l + ilgis / 4
@@ -47,13 +47,58 @@ def dalijimoPusiau():
         if ilgis < epsilon:
             break
 
-    print(f"Dalijimo pusiau metodo alogritmas rado atsakymas per {interacijos} iteracijas.")
+    print(f"Dalijimo pusiau metodo alogritmas rado atsakymą per {iteracijos} iteracijas.")
     print(f"su ε = {epsilon}, ats.: {xm}")
     pl.plot(xm, tiklsoF(xm), 'or')
+
+def auksinioPjuvio():
+    global intervalas, epsilon
+    l, r = copy.deepcopy(intervalas[0]), copy.deepcopy(intervalas[1])
+
+    phi = (math.sqrt(5) - 1) / 2
+
+    ilgis = r - l
+    x1 = r - phi*ilgis
+    x2 = l + phi*ilgis
+
+    fx1 = tiklsoF(x1)
+    fx2 = tiklsoF(x2)
+
+    iteracijos = 0
+    while True:
+        iteracijos += 1
+        if fx1 > fx2:
+            l = x1
+            ilgis = r - l
+
+            x1 = x2
+            fx1 = fx2
+            x2 = l + phi * ilgis
+            fx2 = tiklsoF(x2)
+        else:
+            r = x2
+            ilgis = r - l
+
+            x2 = x1
+            fx2 = fx1
+            x1 = r - phi * ilgis
+            fx1 = tiklsoF(x1)
+
+        if ilgis < epsilon:
+            break
+
+    if fx1 < fx2: ats = (x1, fx1)
+    else: ats = (x2, fx2)
+
+    print(f"Auksinio pjūvio metodo alogritmas rado atsakymą per {iteracijos} iteracijas.")
+    print(f"su ε = {epsilon}, ats.: {ats[1]}")
+    pl.plot(ats[0], ats[1], 'og')
+
 
 x = np.arange(intervalas[0], intervalas[1], 0.1)
 pl.plot(x, tiklsoF(x))
 
 dalijimoPusiau()
+auksinioPjuvio()
 
 pl.show()
